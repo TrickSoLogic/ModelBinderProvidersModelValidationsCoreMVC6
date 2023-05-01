516830328180e175c43562c0dae80ee5ab9f113a
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 22:21:16 +0530

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinderProvidersModelValidationsCoreMVC6.Models;
+using ModelBinderProvidersModelValidationsCoreMVC6.CustomModelBinders;
+
+namespace ModelBinderProvidersModelValidationsCoreMVC6.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("register")]
+    //Example JSON: { "PersonName": "William", "Email": "william@example.com", "Phone": "123456", "Password": "william123", "ConfirmPassword": "william123" }
+    //[ModelBinder(BinderType = typeof(PersonModelBinder))]
+    public IActionResult Index(Person person)
+    {
+      if (!ModelState.IsValid)
+      {
+        //get error messages from model state
+        string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+  
+        return BadRequest(errors);
+      }
+
+      return Content($"{person}");
+    }
+  }
+}
---
+++ ModelValidationsExample/CustomModelBinders/PersonBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using ModelBinderProvidersModelValidationsCoreMVC6.Models;
+
+namespace ModelBinderProvidersModelValidationsCoreMVC6.CustomModelBinders
+{
+  public class PersonBinderProvider : IModelBinderProvider
+  {
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+      if (context.Metadata.ModelType == typeof(Person))
+      {
+        return new BinderTypeModelBinder(typeof(PersonModelBinder));
+      }
+      return null;
+    }
+  }
+}
